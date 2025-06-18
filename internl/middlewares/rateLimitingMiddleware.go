1d353635135ea330dfc35115381d739c8319ebbd
Moukhtar Youssef Moukhtar Elsayed Abdelnabi <101397758+Moukhtar-youssef@users.noreply.github.com>
2025-06-18 12:38:28 +0200

    .

--- internl/middlewares/rateLimitingMiddleware.go
+++ internl/middlewares/rateLimitingMiddleware.go
@@ -1,1 +1,19 @@
 package middlewares
+
+import (
+	"sync"
+	"time"
+)
+
+type visitor struct {
+	tokens     int
+	lastRefill time.Time
+	mu         sync.Mutex
+}
+
+type ratelimiter struct {
+	visitors map[string]*visitor
+	rate     int
+	window   time.Duration
+	mu       sync.Mutex
+}
