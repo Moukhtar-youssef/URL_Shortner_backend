ea4f8482443aa68b7fac77ac85148841f7dfd22b
Moukhtar Youssef Moukhtar Elsayed Abdelnabi <101397758+Moukhtar-youssef@users.noreply.github.com>
2025-06-25 22:40:02 +0300

    Deleting unnessecary files and making the error from the rate limiter more detailed

--- internl/middlewares/rateLimitingMiddleware.go
+++ internl/middlewares/rateLimitingMiddleware.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"fmt"
+	"math"
 	"net"
 	"net/http"
 	"sync"
@@ -45,7 +47,7 @@ func (rl *Ratelimiter) getVisitor(ip string) *visitor {
 	return v
 }
 
-func (rl *Ratelimiter) allow(ip string) bool {
+func (rl *Ratelimiter) allow(ip string) (bool, time.Duration) {
 	v := rl.getVisitor(ip)
 
 	v.mu.Lock()
@@ -59,11 +61,11 @@ func (rl *Ratelimiter) allow(ip string) bool {
 	}
 
 	if v.tokens <= 0 {
-		return false
+		return false, elapsed
 	}
 
 	v.tokens--
-	return true
+	return true, elapsed
 }
 
 func (rl *Ratelimiter) cleanup() {
@@ -98,12 +100,28 @@ func RateLimitMiddleware(rl *Ratelimiter) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ip := getIP(r)
 
-			if !rl.allow(ip) {
-				http.Error(w, "Too many Requests", http.StatusTooManyRequests)
+			allowed, elapsed := rl.allow(ip)
+			remaining := rl.window - elapsed
+			retry := formateRetryString(remaining)
+			if !allowed {
+				Error_msg := fmt.Sprintf("Too many Requests please try again after %s", retry)
+				http.Error(w, Error_msg, http.StatusTooManyRequests)
 				return
 			}
 
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func formateRetryString(window time.Duration) string {
+	seconds := int(window.Seconds())
+	if seconds <= 60 {
+		return fmt.Sprintf("%ds", seconds)
+	} else if seconds <= 3600 {
+		return fmt.Sprintf("%dm", int(math.Round(window.Minutes())))
+	} else if seconds <= 86400 {
+		return fmt.Sprintf("%dh %dm", int(math.Round(window.Hours())), int(math.Round(window.Minutes()))%60)
+	}
+	return fmt.Sprintf("%dd %dh", int(math.Round(window.Hours()))/24, int(math.Round(window.Hours()))%24)
 }
